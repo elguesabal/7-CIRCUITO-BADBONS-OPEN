d7791acc2e38cf31bb50566cb5c02c1262ad9ce8
elguesabal <joseelguesabal@gmail.com>
2024-08-04 23:23:46 -0300

    enserindo requisicao a tabela e botando na ordem decrescente de vitorias

--- javascript/tabelas.js
+++ javascript/tabelas.js
@@ -1,30 +1,22 @@
 const url = new URL(window.location.href);
 const categoria = url.searchParams.get("categoria");
 const modalidade = url.searchParams.get("modalidade");
-const iframe = document.querySelector(".iframe");
 
-if (categoria == "sub13") {
-	// (modalidade == "simples masculina") ? iframe.src = "": undefined;
-	// (modalidade == "simples feminina") ? iframe.src = "" : undefined;
-	// (modalidade == "dupla mista") ? iframe.src = "" : undefined;
-
-	(modalidade == "simples masculina") ? alert("categoria: sub13\nmodalidade: simples masculina") : undefined;
-	(modalidade == "simples feminina") ? alert("categoria: sub13\nmodalidade: simples feminina") : undefined;
-	(modalidade == "dupla mista") ? alert("categoria: sub13\nmodalidade: dupla mista") : undefined;
-} else if (categoria == "sub17") {
-	// (modalidade == "simples masculina") ? iframe.src = "": undefined;
-	// (modalidade == "simples feminina") ? iframe.src = "" : undefined;
-	// (modalidade == "dupla mista") ? iframe.src = "" : undefined;
-
-	(modalidade == "simples masculina") ? alert("categoria: sub17\nmodalidade: simples masculina") : undefined;
-	(modalidade == "simples feminina") ? alert("categoria: sub17\nmodalidade: simples feminina") : undefined;
-	(modalidade == "dupla mista") ? alert("categoria: sub17\nmodalidade: dupla mista") : undefined;
-} else if (categoria == "master") {
-	// (modalidade == "simples masculina") ? iframe.src = "": undefined;
-	// (modalidade == "simples feminina") ? iframe.src = "" : undefined;
-	// (modalidade == "dupla mista") ? iframe.src = "" : undefined;
-
-	(modalidade == "simples masculina") ? alert("categoria: master\nmodalidade: simples masculina") : undefined;
-	(modalidade == "simples feminina") ? alert("categoria: master\nmodalidade: simples feminina") : undefined;
-	(modalidade == "dupla mista") ? alert("categoria: master\nmodalidade: dupla mista") : undefined;
-}
+fetch("https://api-7-circuito-badbons-open.onrender.com/tabela" + "?categoria=" + categoria + "&modalidade=" + modalidade)
+.then(res => { return res.json(); })
+.then(table => {
+	table.sort((a, b) => b.v - a.v);
+	for(let i = 0; i < table.length; i++) {
+		document.getElementById("table").innerHTML += `
+			<tr>
+				<th scope="row">${i}</th>
+				<td>${table[i].nome}</td>
+				<td>${table[i].tj}</td>
+				<td>${table[i].v}</td>
+				<td>${table[i].d}</td>
+				<td>${table[i].vSet}</td>
+			</tr>
+		`;
+	}
+})
+.catch(error => console.log("Error na tabela", error))
