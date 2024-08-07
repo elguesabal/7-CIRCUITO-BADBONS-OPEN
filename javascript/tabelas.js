f15327d548125009026fae8799aed2c4e2ae3ea6
elguesabal <joseelguesabal@gmail.com>
2024-08-07 00:12:12 -0300

    salvando alteracoes feito nas estruturas de pastas e tbm a juncao dos botoes da pagina inicial

--- javascript/tabelas.js
+++ javascript/tabelas.js
@@ -2,19 +2,23 @@ const url = new URL(window.location.href);
 const categoria = url.searchParams.get("categoria");
 const modalidade = url.searchParams.get("modalidade");
 
-fetch("https://api-7-circuito-badbons-open.onrender.com/tabela" + "?categoria=" + categoria + "&modalidade=" + modalidade)
+// fetch("https://api-7-circuito-badbons-open.onrender.com/tabela" + "?categoria=" + categoria + "&modalidade=" + modalidade)
+fetch("http://localhost:3000/tabela" + "?categoria=" + categoria + "&modalidade=" + modalidade)
 .then(res => { return res.json(); })
 .then(table => {
 	table.sort((a, b) => b.v - a.v);
 	for(let i = 0; i < table.length; i++) {
 		document.getElementById("table").innerHTML += `
 			<tr>
-				<th scope="row">${i + 1}</th>
+				<th scope="row">${i + 1}°</th>
 				<td>${table[i].nome}</td>
 				<td>${table[i].tj}</td>
 				<td>${table[i].v}</td>
 				<td>${table[i].d}</td>
 				<td>${table[i].vSet}</td>
+				<td>${table[i].pf}</td>
+				<td>${table[i].ps}</td>
+				<td>${table[i].pf - table[i].ps}</td>
 			</tr>
 		`;
 	}
